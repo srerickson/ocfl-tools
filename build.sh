a4f4ecc440549e723181f0304a53841419d47468
Seth R Erickson <sr.erickson@gmail.com>
2024-09-06 11:47:31 -0700

    fix build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ podman buildx build --rm --platform linux/arm64,linux/amd64  \
 	--manifest ocfl-tools-image .
 
 # # push to docker
-# podman manifest push ocfl-tools-image "docker://srerickson/ocfl-tools:latest"
+podman manifest push ocfl-tools-image "docker://srerickson/ocfl-tools:latest"
 
 # # remove the manifest
-# podman manifest rm ocfl-tools-image
+podman manifest rm ocfl-tools-image
