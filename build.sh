7a82bb66300360156ded95df66dd8790a3191d1e
Seth R Erickson <sr.erickson@gmail.com>
2024-09-06 11:24:02 -0700

    add version command with semver, buildtime, and commit

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 podman manifest create ocfl-tools-image
 
 # build with buildx
-podman buildx build --rm --platform linux/arm64,linux/amd64  \
+podman buildx build --rm --platform linux/amd64  \
+	--build-arg OCFLTOOLS_VERSION=$(cat VERSION) \
+	--build-arg OCFLTOOLS_BUILDTIME=$(date +"%Y%m%d.%H%M%S") \
 	-t srerickson/ocfl-tools:latest \
 	--manifest ocfl-tools-image .
 
-# push to docker
-podman manifest push ocfl-tools-image "docker://srerickson/ocfl-tools:latest"
+# # push to docker
+# podman manifest push ocfl-tools-image "docker://srerickson/ocfl-tools:latest"
 
-# remove the manifest
-podman manifest rm ocfl-tools-image
+# # remove the manifest
+# podman manifest rm ocfl-tools-image
