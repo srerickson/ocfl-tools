40a20d74c1e6bf07a16c22d5b3d19a0354bb1806
Seth Erickson <sr.erickson@gmail.com>
2025-02-23 16:26:54 +0000

    wip

--- cmd/ocfl/internal/stage/stagefile_test.go
+++ cmd/ocfl/internal/stage/stagefile_test.go
@@ -2,8 +2,9 @@ package stage_test
 
 import (
 	"context"
-	"maps"
-	"slices"
+	"io/fs"
+	"os"
+	"path"
 	"strings"
 	"testing"
 
@@ -32,7 +33,7 @@ func TestStageFile_AddDir(t *testing.T) {
 			be.NilErr(t, err)
 			err = changes.AddDir(ctx, contentFixture)
 			be.NilErr(t, err)
-			be.False(t, stageIncludesHidden(changes))
+			stageStateMachesDir(t, changes, contentFixture, false, ".")
 		})
 
 		t.Run("with hidden", func(t *testing.T) {
@@ -40,7 +41,7 @@ func TestStageFile_AddDir(t *testing.T) {
 			be.NilErr(t, err)
 			err = changes.AddDir(ctx, contentFixture, stage.AddWithHidden())
 			be.NilErr(t, err)
-			be.True(t, stageIncludesHidden(changes))
+			stageStateMachesDir(t, changes, contentFixture, true, ".")
 		})
 
 		t.Run("with digest jobs", func(t *testing.T) {
@@ -48,7 +49,28 @@ func TestStageFile_AddDir(t *testing.T) {
 			be.NilErr(t, err)
 			err = changes.AddDir(ctx, contentFixture, stage.AddDigestJobs(2))
 			be.NilErr(t, err)
+			stageStateMachesDir(t, changes, contentFixture, false, ".")
 		})
+
+		t.Run("with remove", func(t *testing.T) {
+			changes, err := stage.NewStageFile(newObj, "sha512")
+			be.NilErr(t, err)
+			err = changes.AddDir(ctx, contentFixture, stage.AddAndRemove())
+			be.NilErr(t, err)
+			stageStateMachesDir(t, changes, contentFixture, false, ".")
+		})
+
+		t.Run("with multiple", func(t *testing.T) {
+			changes, err := stage.NewStageFile(newObj, "sha512")
+			be.NilErr(t, err)
+			err = changes.AddDir(ctx, contentFixture, stage.AddWithHidden(), stage.AddAs("tmp"), stage.AddAndRemove())
+			be.NilErr(t, err)
+			stageStateMachesDir(t, changes, contentFixture, true, "tmp")
+		})
+
+		//t.Run("duplicate", func(t *testing.T) {
+
+		//})
 
 	})
 
@@ -86,13 +108,46 @@ func TestStageFile_AddDir(t *testing.T) {
 	})
 }
 
-func stageIncludesHidden(s *stage.StageFile) bool {
-	names := slices.Collect(maps.Keys(s.NextState))
-	for _, n := range names {
-		for _, part := range strings.Split(n, "/") {
-			if strings.HasPrefix(part, ".") {
-				return true
-			}
+func stageStateMachesDir(t *testing.T, s *stage.StageFile, dir string, withHidden bool, as string) {
+	t.Helper()
+	count := 0
+	walkErr := fs.WalkDir(os.DirFS(dir), ".", func(p string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		if !withHidden && isHidden(p) {
+			return nil
+		}
+		count++
+		p = path.Join(as, p)
+		if s.NextState[p] == "" {
+			t.Error("stage doesnt include", p)
+		}
+		return nil
+	})
+	be.NilErr(t, walkErr)
+	if l := len(s.NextState); l != count {
+		t.Errorf("stage doesn't have same number of files as content fixture; want=%d, got=%d", count, l)
+	}
+}
+
+// func stageIncludesHidden(s *stage.StageFile) bool {
+// 	names := slices.Collect(maps.Keys(s.NextState))
+// 	for _, n := range names {
+// 		if isHidden(n) {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+func isHidden(n string) bool {
+	for _, part := range strings.Split(n, "/") {
+		if strings.HasPrefix(part, ".") {
+			return true
 		}
 	}
 	return false
