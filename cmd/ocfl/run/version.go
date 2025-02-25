5c57c01de8356636fbd4a767b7dc2fd013b70a6b
Seth Erickson <sr.erickson@gmail.com>
2025-02-25 14:57:23 -0800

    bump version (#42)

--- cmd/ocfl/run/version.go
+++ cmd/ocfl/run/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// ocfl-tools version
-	Version   = "0.1.3" // may be set by -ldflags
+	Version   = "0.2.0" // may be set by -ldflags
 	BuildTime string    // always set by -ldflags
 )
 
